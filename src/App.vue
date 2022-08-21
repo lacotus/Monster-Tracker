e35bbed3025b35513388825af20985e8a8490296
lacotus <lspiessebersole@gmail.com>
2022-08-21 19:43:14 -0400

    Deleted the HelloWorld component and added my own, called MainHeader.vue. This is where I will likely keep the title of the page, as well as several buttons. Yet to be decided.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainHeader
   }
 }
 </script>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Hello world</div>
+</template>
+
+<script>
+export default {
+	name: 'MainHeader',
+	props: {
+		title: String
+	}
+}
+</script>
+
+<style></style>
