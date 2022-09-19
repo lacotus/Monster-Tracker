b46a660ec6e374d0dceb453df824ac61c64e0599
lacotus <lspiessebersole@gmail.com>
2022-09-18 20:02:00 -0400

    Adding vim syntax highting.

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>Hello world</div>
+    <div class="MainStyle">Monster Tracker</div>
+    <div><button class="ButtonStyle" v-on:click="writeWindowWidth()"></button></div>
 </template>
+
+<!-- Commenting some code for testing reasons -->
 
 <script>
 export default {
 	name: 'MainHeader',
 	props: {
 		title: String
+	},
+	methods: {
+		writeWindowWidth(){
+			console.log(window.innerWidth);
+			console.log(window.innerHeight);
+		}
 	}
 }
 </script>
 
-<style></style>
+<style>
+    .ButtonStyle {
+		width: 50px;
+		height: 50px;	
+    }
+
+    .MainStyle {
+		background-color: black;
+		color: white;
+    }
+</style>
---
+++ src/components/btnComponent.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script>
+export default {
+    name: 'btnComponent',
+    props: {
+	text: String
+    }
+}
+</script>
+
+<style></style>
+
