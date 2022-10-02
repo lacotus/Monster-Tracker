6a9af8d5329a26f831ca11ca0f60bb09df90db4e
lacotus <lspiessebersole@gmail.com>
2022-10-02 19:57:03 -0400

    Further worked on MonsterArea.vue, specifically I have changed the html around so that the container wraps AROUND the flex-box, so that the flex-box does not need to expand to the floor, the container div is doing so via the calucations performed in the scripting section. This means that the flex-box is not filling the width / height, but since there are no borders around the fb itself it is okay. Still need to figure out how to add / remove the MonsterInstance components.

--- src/components/MonsterArea.vue
+++ src/components/MonsterArea.vue
@@ -1,15 +1,22 @@
 <template>
-	<div id="main" class="main" v-bind:style="MainHeightStyle">Monster Area</div>
+
+	<div id="monsterAreaContainer" class="container">
+		<div id="flexBox" class="flexBox">
+			<component :is="component"></component>
+			<Button class="tempButton">Temp button</Button>
+		</div>
+	</div>
+
 </template>
 
 <script>
 
-import MonsterInstance from './MonsterInstance.vue'
-
 export default {	
 	name: 'MonsterArea',
-	components: {
-		MonsterInstance
+	computed: {
+		component: function() {
+			return 'monsterInstance';
+		}
 	},
 	created: function() {
 		window.addEventListener("resize", this.calcHeight);
@@ -21,15 +28,12 @@ export default {
 			// height to the container div
 			var mainHeight = window.innerHeight;
 			mainHeight = mainHeight - 125; /* MainTitle height plus a little extra to account for padding */
-			document.getElementById('main').style.height = mainHeight + 'px';
+			document.getElementById('monsterAreaContainer').style.height = mainHeight + 'px';
 			console.log('Window Height: ' + mainHeight);
 		},
 
 		addMonster() {
-			var instance = new MonsterInstance();
-			instance.$mount()
-			var rent = document.getElementById('main')
-			rent.appendChild(instance.$el)
+			
 		}
 	},
 	mounted: function()  {
@@ -48,6 +52,12 @@ export default {
 
 <style>
 	.container {
+		border-color: black;
+		border-style: solid;
+		border-width: 3px;
+	}
+
+	.flexBox {
 		display: flex;
 		flex-flow: row wrap;
 	}
@@ -59,6 +69,10 @@ export default {
 		display: flex;
 		flex-direction: column;
 		flex: 1 1 auto;
+	}
+	.tempbutton {
+		width: 50px;
+		height: 50px;
 	}
 </style>
 
