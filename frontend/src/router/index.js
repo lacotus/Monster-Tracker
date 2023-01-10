37d6c7b66625cd57e192eb0d5a5f9aada74ff20a
lacotus <lspiessebersole@gmail.com>
2023-01-09 22:45:45 -0500

    Followed video, need to figure out how he implements <rotuer-vue> in his App.vue file.

---
+++ frontend/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Register from '@/components/Register'
+
+Vue.use(Router)
+
+export default new Router({
+	routes: [
+		{
+			path: '/register',
+			name: 'register',
+			componet: Register
+		}
+	]
+})
+
